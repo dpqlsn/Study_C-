560389a39148ded25f11ab22cb4a1c706164f227
dpqlsn <a01037111209@gmail.com>
2025-02-20 22:51:36 +0900

    auto

---
+++ 04.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std; 
+
+int main(){
+
+    auto n = 100; // n은 int 값으로 초기화
+    auto x = 1.5; // x는 float 값으로 초기화
+    auto y = 1.3e12L; // long long 값으로 초기화
+
+    // 복잡한 변수형을 다룰때 좋게 사용되지만 맹신하다보면 의도찮은 곳에서 오류가 발생하기도 함
+    // 보통  타입 자체가 타이핑하기 애매할 때 사용하고 함수의 매개변수, 구조체나 클래스의 멤버변수로 사용이 불가
+
+    return 0;
+}
