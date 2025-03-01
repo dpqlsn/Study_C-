e160de4b592a3252f566695fe54643da7c02c497
dpqlsn <a01037111209@gmail.com>
2025-03-01 16:17:08 +0900

    상속의 기본 개념

--- 상속/time.h
+++ 상속/time.h
@@ -2,6 +2,12 @@
 #ifndef TIMEH
 #define TIMEH
 
+/* 상속의 기능
+1. 기존의 클래스에 새로운 기능을 추가할 수 있음
+2. 클래스가 나타내고 있는 데이터에 다른 것을 더 추가할 수 있음
+3. 클레스 메서드가 동작하는 방식을 변경할 수 있음
+*/
+
 class Time
 {
 private:
@@ -24,11 +30,17 @@ public:
 };
 
 class NewTime : public Time {
+    /*
+    1. 파생 클래스형의 객체 안에는 기초 클래스형의 데이터 멤버들이 저장됨
+    2. 파생 클래스형의 객체는 기초 클래스형의 메서드들을 이용할 수 있음
+    3. 파생 클래스는 자기 자신의 생성자를 필요로 함
+    4. 파생 클래스는 부가적인 데이터 멤버들과 멤버 함수들을 임의로 추가할 수 있음
+    */
 private:
 	int day;
 public:
-	NewTime();
-	NewTime(int, int, int);
+	NewTime(); // 매개변수가 하나도 없음
+	NewTime(int, int, int); // 매개변수가 세개일때
 	void print();
 	
 };
